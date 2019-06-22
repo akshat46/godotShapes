4351c9f07d8650c5c200f5a2c49d98638e743e84
Akshat Sharma <akshat@Akshats-MacBook-Pro.local>
2019-06-21 17:13:41 -0700

    initial commit

---
+++ KinematicBody2D.gd
@@ -0,0 +1,68 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+const MAX_VELOCITY = Vector2(300, 40)
+const ACCELERATION = Vector2(40, 4)
+const G = 20
+const INIT_VEL = 250
+const JMP_HEIGHT = 460
+
+var motion = Vector2()
+var velocity = Vector2()
+var friction = false
+var collision
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+
+	motion.y += G
+	
+	if is_on_floor():
+		if Input.is_action_pressed("ui_space"):
+			motion.y = -JMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.3)
+	else:
+		motion.x = lerp(motion.x, 0, 0.05)
+	
+	movement_handler()
+#	if Input.is_action_pressed("ui_right"):
+#		motion.x = INIT_VEL
+#	elif Input.is_action_pressed("ui_left"):
+#		motion.x = -INIT_VEL
+	#motion.bounce(collision.normal)
+	
+	velocity = move_and_slide(motion,UP )
+#	move_and_collide(motion*delta)
+	
+func movement_handler():
+	if Input.is_action_pressed("ui_right"):
+		if (velocity.x == 0):
+			motion.x = INIT_VEL
+		else: # acceleration right
+			motion.x = min(velocity.x+ACCELERATION.x, MAX_VELOCITY.x)
+		
+	elif Input.is_action_pressed("ui_left"):
+		if (velocity.x == 0):
+			motion.x = -INIT_VEL
+		else: # acceleration left
+			motion.x = max(velocity.x-ACCELERATION.x, -MAX_VELOCITY.x)
+		
+	elif velocity.x != 0:
+#		deaccelerate()
+		friction = true
+	else:
+		motion.x = 0
+	pass
+	
+#func deaccelerate():
+#	if Input.is_action_just_released("ui_left") or deacc_left: # deaccelaration left
+#		deacc_left = true
+#		motion.x = min(velocity.x+ACCELERATION.x, 0)	
+#	elif Input.is_action_just_released("ui_right") or deacc_right: # deaccelaration right
+#		deacc_right = true
+#		motion.x = max(velocity.x-ACCELERATION.x, 0)
+
